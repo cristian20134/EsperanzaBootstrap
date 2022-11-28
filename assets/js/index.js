8c88da8a793073558ae0acb71c20c0d9b8a65dea
cristian20134 <ctoronaruto@gmail.com>
2022-11-28 19:10:53 -0300

    modficacion de margen menu y animación logo

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.logo-scroll', {
+    duration: 3000,
+    origin:'bottom',
+    distance:'-75px',
+    rotate: {
+        x: 1,
+        y: 180,
+    }
+});
+
+
+sr.reveal('.footer-img', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-350px',
+    rotate: {
+        x: 1,
+        y: 180,
+    }
+});
